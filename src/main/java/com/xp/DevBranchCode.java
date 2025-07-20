1b9b59e168cd0a4a6d4d15ba5063ec3b4a2ffef3
singyi <singyiyo@gmail.com>
2025-07-20 17:04:29 +0800

    merge test:dev code 1111

---
+++ src/main/java/com/xp/DevBranchCode.java
@@ -0,0 +1,12 @@
+package com.xp;
+
+public class DevBranchCode {
+    public static void main(String[] args) {
+        System.out.println("Dev Code 1111");
+        System.out.println("Dev Code 1111");
+        System.out.println("Dev Code 1111");
+        System.out.println("Dev Code 1111");
+        System.out.println("Dev Code 1111");
+        System.out.println("Dev Code 1111");
+    }
+}
