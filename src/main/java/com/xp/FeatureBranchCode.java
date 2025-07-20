35ef7086961fd8ab8a5fe49de7daec0d04b83d8c
singyi <singyiyo@gmail.com>
2025-07-20 18:26:24 +0800

    feature test:feature code 111

---
+++ src/main/java/com/xp/FeatureBranchCode.java
@@ -0,0 +1,11 @@
+package com.xp;
+
+public class FeatureBranchCode {
+    public static void main(String[] args) {
+        System.out.println("Feature Branch Code 111");
+        System.out.println("Feature Branch Code 111");
+        System.out.println("Feature Branch Code 111");
+        System.out.println("Feature Branch Code 111");
+        System.out.println("Feature Branch Code 111");
+    }
+}
