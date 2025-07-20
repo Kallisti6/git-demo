183c31a1016a9ecdc84a81935a58529a9bc1f174
singyi <singyiyo@gmail.com>
2025-07-20 14:40:26 +0800

    添加student类

--- src/main/java/com/xp/Main.java
+++ src/main/java/com/xp/Main.java
@@ -3,5 +3,6 @@ package com.xp;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World! git demo");
+        System.out.println("Hello, World! modify ");
     }
 }
---
+++ src/main/java/com/xp/Student.java
@@ -0,0 +1,9 @@
+package com.xp;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("new a student class");
+    }
+}
