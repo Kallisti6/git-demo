06fbf7fe98890446e923188423b5e0f2d09bf750
singyi <singyiyo@gmail.com>
2025-07-20 14:55:09 +0800

    2

--- src/main/java/com/xp/Main.java
+++ src/main/java/com/xp/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World! git demo");
         System.out.println("Hello, World! modify ");
+
+        System.out.println("Hello, World! modify 2");
     }
 }
--- src/main/java/com/xp/Student.java
+++ src/main/java/com/xp/Student.java
@@ -4,5 +4,7 @@ package com.xp;
 public class Student {
     public static void main(String[] args) {
         System.out.println("new a student class");
+
+        System.out.println("modify by xp");
     }
 }
